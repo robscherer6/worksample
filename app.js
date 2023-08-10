68c15a0029fbcf91dc23baa53da651a91858016b
Rob <robscherer6@gmail.com>
2023-08-09 22:36:56 -0400

    2nd commit, computer restart required

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+function greetUser () {
+    alert('Hello User. Welcome to my web app!')
+}
+
+greetUser();
+
+function add (a, b) {
+    return a + b;
+}
+
+console.log(add(4, 3));
